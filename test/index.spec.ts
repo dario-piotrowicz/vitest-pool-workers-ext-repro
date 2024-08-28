22f8db2b28e046ff05d58b4be2517131b09b2873
Dario Piotrowicz <dario@cloudflare.com>
2024-08-28 12:57:16 +0100

    reproduce issue

---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+import { it, expect } from "vitest";
+
+// Comment the following import to make the test pass
+import "discord-api-types/v10";
+
+it("1 + 1 = 2", async () => {
+  expect(1 + 1).toEqual(2);
+});
